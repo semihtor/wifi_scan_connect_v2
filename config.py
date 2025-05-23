cf6d68d7a5b37984c7889d20a23f9da6a9925f69
Semih Tor <82044766+semihtor@users.noreply.github.com>
2025-05-23 23:37:39 +0300

    Update config.py
    Changes to support the horizontal scroll functionality

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
 # --- Project Settings ---
-WIFI_PASSWORD = "12341234"
+WIFI_PASSWORD = "password"
 START_BUTTON_GPIO = 16
 STOP_BUTTON_GPIO = 26
 ROTARY_ENCODER_A_GPIO = 17
@@ -11,6 +11,8 @@ ROTARY_ENCODER_BUTTON_GPIO = 27
 # OLED Display
 OLED_WIDTH = 128
 OLED_HEIGHT = 64
+OLED_LINE_MAX_CHARS = 18  # Max characters per line on OLED
+OLED_SCROLL_DELAY = 0.30  # Scroll speed in seconds
 
 # Network
 HOSTNAME_PREFIX = "RPi0-"
